014bca118d794954b55d707f8dcc49b3bf52b63b
Fer <fgarcia@mail.wm.edu>
2019-03-04 16:54:09 -0500

    added resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,7 +1,49 @@
 import React, {Component} from "react";
 import "./style.css";
-import { Document, Page } from 'react-pdf';
+// import { Document, Page } from 'react-pdf';
 
+
+// class Resume extends Component {
+
+//   state = { 
+//     numPages: null, 
+//     pageNumber: 1,
+//   };
+
+//   onDocumentLoadSuccess = ({ numPages }) => {
+//     this.setState({ numPages });
+//   }
+//   render() {
+//     const { pageNumber, numPages } = this.state;
+
+//     return (
+
+//       <div id="mainContainer">
+//         <div className="container">
+//           <div className="row">
+//             <div className="col-md-2">
+//             </div>
+//             <div className="col-md-8">
+//               <div className="jumbotron">
+//                 <div style={{ width: 600 }}>
+//                   <Document
+//                     file="../img/Resume.pdf"
+//                     onLoadSuccess={this.onDocumentLoadSuccess}
+//                   ><Page pageNumber={pageNumber}
+//                     width={600} /></Document>
+//                 </div>
+//               </div>
+//             </div>
+//             <div className="col-md-2">
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+
+//     );
+//   }
+// }
+// export default Resume;
 
 class Resume extends Component {
 
@@ -21,21 +63,16 @@ class Resume extends Component {
       <div id="mainContainer">
         <div className="container">
           <div className="row">
-            <div className="col-md-2">
+            <div className="col-md-1">
             </div>
-            <div className="col-md-8">
+            <div className="col-md-10">
               <div className="jumbotron">
-                <div style={{ width: 600 }}>
-                  <Document
-                    file="../../components/img/Resume.pdf"
-                    onLoadSuccess={this.onDocumentLoadSuccess}
-                  ><Page pageNumber={pageNumber}
-                    width={600} /></Document>
-                    <p>Page {pageNumber} of {numPages}</p>
+                <div >
+                  <img id="resume" src= {require("../img/resume.jpg")}></img>
                 </div>
               </div>
             </div>
-            <div className="col-md-2">
+            <div className="col-md-1">
             </div>
           </div>
         </div>
